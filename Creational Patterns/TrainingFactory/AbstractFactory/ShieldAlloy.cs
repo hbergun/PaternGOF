1b0be2988dadb04f84184757957aeab084eec1a9
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-14 01:49:55 +0300

    Shield Added

---
+++ Creational Patterns/TrainingFactory/AbstractFactory/ShieldAlloy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    public class ShieldAlloy : ITankShield
+    {
+        public string Defence()
+        {
+            return "Alloy Shield Protected Tank";
+        }
+    }
+}
---
+++ Creational Patterns/TrainingFactory/AbstractFactory/ShieldCarbon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    public class ShieldCarbon : ITankShield
+    {
+        public string Defence()
+        {
+            return "Carbon Shield Protected Tank";
+        }
+    }
+}
