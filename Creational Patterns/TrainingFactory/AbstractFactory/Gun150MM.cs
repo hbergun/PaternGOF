01bbd66d5ca33e45719b2474373528da74857a8d
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-14 01:46:01 +0300

    TrainFactory Added

---
+++ Creational Patterns/TrainingFactory/AbstractFactory/Gun150MM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    public class Gun150MM : ITankGun
+    {
+        public string Fire()
+        {
+            return "Gun150MM Fired";
+        }
+    }
+}
---
+++ Creational Patterns/TrainingFactory/AbstractFactory/Gun80MM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    public class Gun80MM : ITankGun
+    {
+        public string Fire()
+        {
+            return "Gun80MM Fired";
+        }
+    }
+}
