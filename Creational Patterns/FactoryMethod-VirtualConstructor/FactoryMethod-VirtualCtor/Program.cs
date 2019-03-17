b37a053d369921f2623cba3a36872efa635ed001
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-17 20:17:32 +0300

    Git Init

---
+++ Creational Patterns/FactoryMethod-VirtualConstructor/FactoryMethod-VirtualCtor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryMethod_VirtualCtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
