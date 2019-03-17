65b2a77ace2b235cb774ee15f9e30c6db630c0b1
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-17 15:11:03 +0300

    Pattern is Completed

--- Creational Patterns/TrainingFactory/AbstractFactory/LeopardTankFactory.cs
+++ Creational Patterns/TrainingFactory/AbstractFactory/LeopardTankFactory.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AbstractFactory
 {
-    public class LeoparkTankFactory : ITankFactory
+    public class LeopardTankFactory : ITankFactory
     {
         public Tank GetTank()
         {
