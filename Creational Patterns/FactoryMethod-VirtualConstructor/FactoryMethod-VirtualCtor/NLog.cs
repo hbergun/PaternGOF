3d4f2333443fdcd46240bdae8c87a168b335813b
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-17 20:41:51 +0300

    Logger And LoggerFactory Added

---
+++ Creational Patterns/FactoryMethod-VirtualConstructor/FactoryMethod-VirtualCtor/NLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod_VirtualCtor
+{
+    public class NLog : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("NLog Logged");
+        }
+    }
+}
---
+++ Creational Patterns/FactoryMethod-VirtualConstructor/FactoryMethod-VirtualCtor/Serilog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod_VirtualCtor
+{
+    public class Serilog : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Serilog Logged");
+        }
+    }
+}
