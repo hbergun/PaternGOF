65b97b0df9ac7bcda27ea903920b450f8af68b29
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-20 00:52:57 +0300

    Car Builder Added

---
+++ Creational Patterns/Builder/CarBuilder/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarBuilder
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Engine { get; set; }
+        public string Wheel { get; set; }
+
+        public override string ToString()
+        {
+            return $"Brand:{Brand},Engine:{Engine},Wheel{Wheel}";
+        }
+    }
+}
---
+++ Creational Patterns/Builder/CarBuilder/CarBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarBuilder
+{
+    public abstract class CarBuilder
+    {
+        protected Car _car;
+        public Car GetCar
+        {
+            get
+            {
+                return _car;
+            }
+        }
+        public abstract void EngineMontage();
+        public abstract void WheelMontage();
+    }
+}
