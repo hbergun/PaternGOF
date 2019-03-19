12e90bda4720505a491514e565ad02b3b0a3fcc9
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-20 01:08:26 +0300

    Director Added

--- Creational Patterns/Builder/CarBuilder/Mercedes.cs
+++ Creational Patterns/Builder/CarBuilder/Mercedes.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace CarBuilder
 {
-    class Mercedes
+    public class Mercedes : CarBuilder
     {
+        public Mercedes()
+        {
+            _car = new Car { Brand = "Mercedes" };
+        }
+
+        public override void EngineMontage()
+        {
+            _car.Engine = "2.0 TDI";
+        }
+
+        public override void WheelMontage()
+        {
+            _car.Engine = "Continental Premium Contact 5";
+        }
     }
 }
