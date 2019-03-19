c99d601fe1a97d590858873982277dc263979f4a
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-20 01:43:18 +0300

    Email Abstract Class Added Builder Class Added

---
+++ Creational Patterns/Builder/MailBuilder/MailBuilder/EmailAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailBuilder
+{
+    public abstract class EmailAccount
+    {
+        public string ServiceProvider { get; set; }
+        public string MailExtension { get; set; }
+
+        public override string ToString()
+        {
+            return $"Service Provider:{ServiceProvider},Mail Extension:{MailExtension}";
+            //return base.ToString();
+        }
+    }
+}
---
+++ Creational Patterns/Builder/MailBuilder/MailBuilder/EmailBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailBuilder
+{
+    public abstract class EmailBuilder
+    {
+        protected EmailAccount _emailAccount;
+        public EmailAccount GetEmailAccount
+        {
+            get
+            {
+                return _emailAccount;
+            }
+        }
+        public abstract string SetServiceProvider();
+        public abstract string SetMailExtension();
+    }
+}
