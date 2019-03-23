c1511ce0dd8611e3b4fe32412be5663f32d9878b
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-23 21:18:20 +0300

    Abstract Car Class Implemented

--- Creational Patterns/Builder/BuildYourOwnCar/BuildYourOwnCar/Car.cs
+++ Creational Patterns/Builder/BuildYourOwnCar/BuildYourOwnCar/Car.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace BuildYourOwnCar
 {
-    class Car
+    public abstract class Car
     {
+        public Car()
+        {
+            _stockParts = new List<string>();
+            _stockParts.AddRange(new string[] { "Wheel", "Engine", "Exhaust" });
+            _optionalParts = new List<string>();
+        }
+        protected List<string> _stockParts;
+        protected List<string> _optionalParts;
+
+        public List<string> OptionalParts
+        {
+            get
+            {
+                return _optionalParts;
+            }
+            set
+            {
+                _optionalParts = value;
+            }
+        }
     }
 }
