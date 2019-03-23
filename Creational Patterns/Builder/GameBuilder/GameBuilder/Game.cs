88fd7eacbf2c10fd252e172e3da04db112523576
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-23 16:41:28 +0300

    Game Edited

--- Creational Patterns/Builder/GameBuilder/GameBuilder/Game.cs
+++ Creational Patterns/Builder/GameBuilder/GameBuilder/Game.cs
@@ -1,6 +1,16 @@
 ﻿namespace GameBuilder
 {
-    public class Game
+    public abstract class Game
     {
+        public Game(string _season, string _country)
+        {
+            Season = _season;
+            Country = _country;
+        }
+
+        public string Season { get; }
+
+        public string Country{ get; }
+
     }
 }
---
+++ Creational Patterns/Builder/GameBuilder/GameBuilder/LocalGame.cs
@@ -0,0 +1,13 @@
+﻿namespace GameBuilder
+{
+    class LocalGame : Game
+    {
+        public LocalGame(int aiStrength, string season, string country)
+            : base(season, country)
+        {
+            AiStrength = aiStrength;
+        }
+
+        public int AiStrength { get; }
+    }
+}
