ed25658a4b7ec26f8cb1afdb35fb950b1b7a5e54
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-24 15:09:57 +0300

    Audi Class Added

--- Creational Patterns/Builder/BuildYourOwnCar/BuildYourOwnCar/Audi.cs
+++ Creational Patterns/Builder/BuildYourOwnCar/BuildYourOwnCar/Audi.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace BuildYourOwnCar
 {
-    class Audi
+    public class Audi : Car
     {
+        public string Model
+        {
+            get
+            {
+                return base.model;
+            }
+            set
+            {
+                base.model = value;
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"AUDI {model}\n\r");
+            for (int i = 0; i < _stockParts.Count; i++)
+            {
+                sb.AppendLine(_stockParts[i] + "\n\r");
+            }
+            for (int i = 0; i < OptionalParts.Count; i++)
+            {
+                sb.AppendLine(OptionalParts[i] + "\n\r");
+            }
+            return sb.ToString();
+        }
     }
 }
